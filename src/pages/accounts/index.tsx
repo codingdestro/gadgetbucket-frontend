4bd01cc0b85ec79d6be8994080bf261b4b01de70
mohd anas <localmail.office@gmail.com>
2024-02-23 16:17:41 +0530

    update - user account page

--- src/pages/accounts/index.tsx
+++ src/pages/accounts/index.tsx
@@ -6,9 +6,12 @@ const index = () => {
   const changeToggleLogin = () => setToggleLogin((prevState) => !prevState);
   return (
     <div className="field-container">
+      <header>e-comm {toggleLogin ? "signin" : "login"}</header>
       {toggleLogin ? <Signin /> : <Login />}
-      <div className="login-toggle" onClick={changeToggleLogin}>
-        {toggleLogin ? "login" : "signin"}
+      <div className="toggle-link" onClick={changeToggleLogin}>
+        {toggleLogin
+          ? "already have an account login"
+          : "don't have any accout signin"}
       </div>
     </div>
   );
